5630f44c6720b981af140c52df4fa5f033449b26
kristijanpanov <kristijan_panov@hotmail.com>
2020-08-21 10:21:39 +0200

    Update: extracting POS of Entities based on reference dataset gabril

--- src/main/java/org/group/sensim/possequences/POSApp.java
+++ src/main/java/org/group/sensim/possequences/POSApp.java
@@ -14,48 +14,52 @@ import org.group.sensim.eval.FoxBinding;
 import org.group.sensim.eval.reader.NifReader;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.util.List;
+import java.util.Map;
 
 public class POSApp {
 
     private static POSMarker posMarker = new POSMarker();
     public final static Logger log = LogManager.getLogger(POSApp.class);
 
-    public static void main(String[] args) throws MalformedURLException {
+    public static void main(String[] args) throws MalformedURLException, FileNotFoundException, UnsupportedEncodingException {
 
         String sentence = "Nikola Tesla moved to North America.";
         FoxResponse response;
-
-        NifReader nf = new NifReader();
         String testFile = "./src/main/resources/eval/ReutersTest.ttl";
-        List<Document> docs = nf.readData(testFile);
+        extractPOSfromTtl(testFile);
+    }
+
+    //TODO ne gi simplificiri, iskari direkt POS tags recenicite. Vidi na kakov Entity ima kakov POS-Tag.
+    // Napisi go tva u edna lista dokument.
+    //ako ti se malko primerite, najdi uste eden .ttl dokument.
+    //TODO produzi dalje.
+    //TODO napisi si points so sakas da gi razgovaras utre na termino.
+    private static void extractPOSfromTtl(String ttlFile) throws FileNotFoundException, UnsupportedEncodingException {
+        NifReader nf = new NifReader();
+        List<Document> docs = nf.readData(ttlFile);
         SentenceSimplifier ss = SentenceSimplifier.getInstance();
+        PrintWriter writer = new PrintWriter("marking_entityToPOS.txt", "UTF-8");
 
         docs.forEach(basisDoc -> {
-            try {
-                Tree posTree;
+            Tree posTree;
+            //String simpleSentences = ss.simplifyFactualComplexSentence(basisDoc.getText());
 
-                String simpleSentences = ss.simplifyFactualComplexSentence(basisDoc.getText());
-                FoxResponse simplifiedFoxResponse = FoxBinding.sendRequest(simpleSentences);
+            Map<Integer, String> entities = NifReader.getEntities(basisDoc);
+            NifReader.printNifDocument(basisDoc);
 
-                log.info("Extracting POS tags from: [" + simpleSentences + "]");
-                posTree = posMarker.extractPOStags(simpleSentences);
-                log.info("POS Tags: [" + posTree.toString() + "]");
+            log.info("Extracting POS tags from: [" + basisDoc.getText() + "]");
+            posTree = posMarker.extractPOStags(basisDoc.getText());
+            log.info("POS Tags: [" + posTree.toString() + "]");
 
-                posMarker.printPOSofEntities(posTree, simplifiedFoxResponse.getEntities());
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
+            String entityPos = posMarker.extractPOSofEntities(posTree, entities, writer);
         });
 
-//        log.info("Extracting POS tags from: [" + sentence + "]");
-//        posTree = posMarker.extractPOStags(sentence);
-//        log.info("POS Tags: [" + posTree.toString() + "]");
-//
-//        response = FoxBinding.sendRequest(sentence);
-//
-//        posMarker.printPOSofEntities(posTree, response.getEntities());
+        writer.close();
+
     }
 }
--- src/main/java/org/group/sensim/possequences/POSMarker.java
+++ src/main/java/org/group/sensim/possequences/POSMarker.java
@@ -12,9 +12,8 @@ import org.group.sensim.GlobalProperties;
 import org.group.sensim.ParseResult;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.io.PrintWriter;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class POSMarker {
@@ -35,32 +34,38 @@ public class POSMarker {
     }
 
 
-    public void printPOSofEntities(Tree posTree, List<Entity> entities) {
+    public String extractPOSofEntities(Tree posTree, Map<Integer, String> entities, PrintWriter writer) {
         Map<String, String> wordPos = mapWordPOS(posTree);
-
-        for (Entity entity : entities) {
+        String posTag;
+        String entityPos = "";
+        for (String entity : entities.values()) {
             for (String word : wordPos.keySet()) {
-                if (entity.getText().contains(word)) {
-                    System.out.println("The entity: + [" + entity.getText() + "] contains (" + word + ") with the tag: " + wordPos.get(word) );
-                }
+                posTag = wordPos.get(word);
+                if (!(posTag.equals("DT") || posTag.equals("TO") || posTag.equals("IN") || posTag.equals("CC") || posTag.equals("VBZ")))
+                    if (entity.contains(word)) {
+                        entityPos = "The entity: [" + entity + "] contains (" + word + ") with the tag: " + wordPos.get(word);
+                        writer.println(entityPos);
+                    }
             }
         }
+        return entityPos;
     }
 
     /**
-     * Puts (word, posTag) from the tagged tree into a hashmap.
+     * Saves word and its pos-tag <word, posTag> from the tagged tree into a hashmap.
      *
      * @param posTree - the tree containing every pos tag from the sentence.
      * @return HashMap <word(String), posTag(String)>
      */
     private Map<String, String> mapWordPOS(Tree posTree) {
+        //TODO change to list, because Map does not allow duplicates?
         Map<String, String> entityPos = new HashMap<String, String>();
         String tag = "";
         for (Tree word : posTree.getLeaves()) {
-            Tree parent = word.parent(posTree.firstChild());
-            tag = parent.parent(posTree.firstChild()).label().toString();
-            tag += " " + parent.label().toString();
-
+            //Tree parent = word.parent(posTree.firstChild());
+            //last two tags (testing)
+            //tag = parent.parent(posTree.firstChild()).label().toString();
+            tag = word.parent(posTree.firstChild()).label().toString();
             entityPos.put(word.toString(), tag);
         }
 
