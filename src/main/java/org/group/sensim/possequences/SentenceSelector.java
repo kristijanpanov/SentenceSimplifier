f405a7e53987756b9d90aef956329584bacbec91
kristijanpanov <kristijan_panov@hotmail.com>
2020-10-12 20:07:49 +0200

    main-app(for now) for whole simplification and summarization processes.

--- src/main/java/org/group/sensim/possequences/SentenceSelector.java
+++ src/main/java/org/group/sensim/possequences/SentenceSelector.java
@@ -1,14 +1,22 @@
 package org.group.sensim.possequences;
 
 import org.aksw.fox.binding.FoxResponse;
+import org.aksw.gerbil.transfer.nif.Document;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.group.sensim.AnalysisUtilities;
+import org.group.sensim.Question;
+import org.group.sensim.SentenceSimplifier;
 import org.group.sensim.eval.FoxBinding;
+import org.group.sensim.eval.reader.NifReader;
+import org.group.sensim.possequences.relation.SentenceTransformer;
 
 import java.io.*;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This class serves to select the relevant sentences.
@@ -24,28 +32,76 @@ public class SentenceSelector {
         posMarker = new POSMarker();
     }
 
-    public static void main(String[] args) throws MalformedURLException, FileNotFoundException, UnsupportedEncodingException {
+
+
+    public static void main(String[] args){
+        //TODO for now testing simplification. In the future move this in mainApp.
         SentenceSelector selector = new SentenceSelector();
-        //selector.isSentenceRelevant("Nicola Tesla is the all-time best scientist of the whole World.");
+        SentenceTransformer sTransf = new SentenceTransformer();
+        SentenceSimplifier ss = SentenceSimplifier.getInstance();
+        //Map<Question, String> questionSentence = new HashMap<>();
 
-
-        // -----> TODO 1. slegni datasets (newsela, wikismall/large), 2. iterate through them and select sentences as relevant/unrelevant.
-        //TODO ... 2.1 simplify with HiS, then 2.2 mark as Relevant/notRelevant
-        //TODO ... 2.0 simplify na drug nacin, 2.1. HiS (optional), 2.2. mark relevance
-        String corpusFile = "./src/main/resources/datasets/simpleWiki_.ori.test.dst";
+        String corpusFile = "./src/main/resources/datasets/PWKP_108016_SimpleComplexSentencesPair";
         List<String> sentences = extractLinesFromFile(corpusFile);
-        PrintWriter writer = new PrintWriter("resultsFox_RELEVANT_ONLY_simpleWiki.txt", "UTF-8");
 
-        for (String sentence : sentences){
-            if( selector.isSentenceRelevant(sentence) ) {
-                FoxResponse response = FoxBinding.sendRequest(sentence);
-                //if(response.getEntities().size() > 0 ) {
-                    printToFile(writer, response.toString());
-                //}
-            }
-        }
-        writer.close();
+        NifReader nf = new NifReader();
+        String testFile = "./src/main/resources/eval/ReutersTest.ttl";
+        List<Document> docs = nf.readData(testFile);
+
+        docs.forEach(basisDoc -> {
+            Map<Question, String>  questionSentence = ss.simplifyFactualComplexSentenceAsTree(basisDoc.getText());
+                    for (Question q  : questionSentence.keySet()){
+                    //if sentence Relevant, then further processing.
+                    if (selector.isSentenceRelevant(q)){
+                        //further simplifications happen here. This method should return something :)
+                        sTransf.transformer(q.getIntermediateTree());
+                    }
+                }
+        });
+
+
+//        for (String sentence : sentences){
+//            if (sentence.length()>0) {
+//                questionSentence = ss.simplifyFactualComplexSentenceAsTree(sentence);
+//
+//                for (Question q  : questionSentence.keySet()){
+//                    //if sentence Relevant, then further processing.
+//                    if (selector.isSentenceRelevant(q)){
+//
+//                        //further simplifications happen here. This method should return something :)
+//                        sTransf.transformer(q.getIntermediateTree());
+//
+//                    }
+//                }
+//            }
+//        }
+
+
     }
+
+
+//    public static void main(String[] args) throws MalformedURLException, FileNotFoundException, UnsupportedEncodingException {
+//        SentenceSelector selector = new SentenceSelector();
+//        //selector.isSentenceRelevant("Nicola Tesla is the all-time best scientist of the whole World.");
+//
+//        // -----> TODO 1. slegni datasets (newsela, wikismall/large), 2. iterate through them and select sentences as relevant/unrelevant.
+//        //TODO ... 2.1 simplify with HiS, then 2.2 mark as Relevant/notRelevant
+//        //TODO ... 2.0 simplify na drug nacin, 2.1. HiS (optional), 2.2. mark relevance
+//
+//        String corpusFile = "./src/main/resources/datasets/simpleWiki_.ori.test.dst";
+//        List<String> sentences = extractLinesFromFile(corpusFile);
+//        PrintWriter writer = new PrintWriter("resultsFox_RELEVANT_ONLY_simpleWiki.txt", "UTF-8");
+//
+//        for (String sentence : sentences){
+//            if( selector.isSentenceRelevant(sentence) ) {
+//                FoxResponse response = FoxBinding.sendRequest(sentence);
+//                //if(response.getEntities().size() > 0 ) {
+//                    printToFile(writer, response.toString());
+//                //}
+//            }
+//        }
+//        writer.close();
+//    }
 
     public static List<String> extractLinesFromFile(String file) {
         File testFile = new File(new File(file).getAbsolutePath());
@@ -61,6 +117,7 @@ public class SentenceSelector {
         return lines;
     }
 
+
     /**
      * TODO move method to different destination.
      *
@@ -69,7 +126,7 @@ public class SentenceSelector {
      * @param writer
      * @param line
      */
-    public static void printToFile(PrintWriter writer, String line){
+    public static void printToFile(PrintWriter writer, String line) {
         writer.println(line);
     }
 
@@ -89,4 +146,21 @@ public class SentenceSelector {
 
         return relevant;
     }
+
+    /**
+     * Checks if a sentence contains an entity. If yes, marked as relevant and return true.
+     *
+     * @param q - the sentence to be checked.
+     * @return true, if relevant.
+     */
+    private boolean isSentenceRelevant(Question q) {
+        boolean relevant = false;
+        log.info("Checking relevance of sentence from tree [" + AnalysisUtilities.getCleanedUpYield(q.getIntermediateTree()) + "] ...");
+        relevant = posMarker.checkSentenceRelevance(q.getIntermediateTree());
+        log.info("... finished.  --------> Relevant: [" + relevant + "]");
+
+        return relevant;
+    }
+
+
 }
